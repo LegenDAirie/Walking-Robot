67d1f8955dbf56fb0cb5a7e536b2d9226f6e0c39
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2021-08-26 11:04:42 -0700

    Implimented position placement and validation

--- lib/toy_robot/table.ex
+++ lib/toy_robot/table.ex
@@ -1,5 +1,5 @@
 defmodule ToyRobot.Table do
-  defstruct [:north_boundary, :east_boundry]
+  defstruct [:north_boundary, :east_boundary]
 
   alias ToyRobot.Table
 
@@ -9,8 +9,8 @@ defmodule ToyRobot.Table do
   ## Examples
     iex> alias ToyRobot.Table
     ToyRobot.Table
-    iex> table = %Table{north_boundary: 4, east_boundry: 4}
-    %Table{north_boundary: 4, east_boundry: 4}
+    iex> table = %Table{north_boundary: 4, east_boundary: 4}
+    %Table{north_boundary: 4, east_boundary: 4}
     iex> table |> Table.valid_position?(%{north:  4, east: 4})
     true
     iex> table |> Table.valid_position?(%{north: 0, east: 0})
@@ -20,10 +20,10 @@ defmodule ToyRobot.Table do
   """
 
   def valid_position?(
-        %Table{north_boundary: north_boundary, east_boundry: east_boundry},
+        %Table{north_boundary: north_boundary, east_boundary: east_boundary},
         %{north: north, east: east}
       ) do
-        north >= 0 && north <= north_boundary &&
-        east >= 0 && east <= east_boundry
+    north >= 0 && north <= north_boundary &&
+      east >= 0 && east <= east_boundary
   end
 end
